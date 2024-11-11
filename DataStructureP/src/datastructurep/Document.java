fbe5c5844dcf713b8ef8957ee544994f988b194e
ghadee3r <444200420@student.ksu.edu.sa>
2024-11-12 00:35:58 +0300

    proc

--- DataStructureP/src/datastructurep/Document.java
+++ DataStructureP/src/datastructurep/Document.java
@@ -8,13 +8,11 @@ package datastructurep;
 
 /**
  *
- * @author nouraalmadhi
+ * @author ghade
  */
-import java.io.File;
-import java.util.Scanner;
-import java.io.IOException;
+
 public class Document {
-    LinkedList<String> Words= new LinkedList<String>();
+        LinkedList<String> Words= new LinkedList<>();
     int docuID;
 
     public Document(int docuID, LinkedList<String> Words) {
@@ -23,76 +21,4 @@ public class Document {
     }
     
 
-    public static LinkedList<String> loadStopWords(String stopWordsFile) throws IOException {
-        LinkedList<String> stopWords = new LinkedList<>();
-        Scanner scanner = new Scanner(new File(stopWordsFile));
-        while (scanner.hasNextLine()) {
-            String stopWord = scanner.nextLine().trim().toLowerCase();
-            stopWords.insert(stopWord);
-        }
-        scanner.close();
-        return stopWords;
-    }
-
-    private static String removePunctuation(String word) {
-        return word.replaceAll("[^a-zA-Z0-9]", "");
-    }
-
-    public static LinkedList<String> preprocessContent(String content, LinkedList<String> stopWords) {
-        LinkedList<String> wordsList = new LinkedList<>();
-        String[] words = content.toLowerCase().split("\\s+");
-
-        for (int i = 0; i < words.length; i++) {
-            String word = removePunctuation(words[i]);
-            if (!isStopWord(word, stopWords) && !word.isEmpty()) {
-                wordsList.insert(word);
-            }
-        }
-        return wordsList;
-    }
-
-    private static boolean isStopWord(String word, LinkedList<String> stopWords) {
-        stopWords.findFirst();
-        while (stopWords.retrieve() != null) {
-            if (stopWords.retrieve().equals(word)) {
-                return true;
-            }
-            stopWords.findNext();
-        }
-        return false;
-    }
-
-    public static void readDocuments(String fileName, LinkedList<String> stopWords) {
-        try {
-           
-            Scanner scan = new Scanner(new File(fileName));
-            scan.nextLine();
-
-            while (scan.hasNextLine()) {
-                String line = scan.nextLine();
-                String id = line.substring(0, line.indexOf(',')).trim();
-                int docId = Integer.parseInt(id);
-                String docContent = line.substring(line.indexOf(',') + 1).trim();
-
-                LinkedList<String> wordsList = preprocessContent(docContent, stopWords);
-                Document document = new Document(docId, wordsList);
-
-                System.out.print(  document.docuID +" ");
-                System.out.println(document.Words.displayWordsInline());
-            }
-            scan.close();
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
-
-    public static void main(String[] args) {
-        try {
-            LinkedList<String> stopWords = loadStopWords("stop.txt");
-            readDocuments("dataset.csv", stopWords);
-        } catch (IOException e) {
-            System.out.println("Error loading stop-words: " + e.getMessage());
-        }
-    }
-  
 }
---
+++ DataStructureP/src/datastructurep/Processor.java
@@ -0,0 +1,120 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package datastructurep;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ghade
+ */
+public class Processor {
+LinkedList <String> stopW;
+Index index;
+InvertedIndex invertedind;
+
+public Processor()
+{
+    stopW = new LinkedList<>();
+    index = new Index();
+    invertedind = new InvertedIndex();
+}
+
+
+public void loadStopWords(String stopWordsFile) {
+    Scanner scanner = null;
+    try {
+        scanner = new Scanner(new File(stopWordsFile));
+        while (scanner.hasNextLine()) {
+            String stopWord = scanner.nextLine().trim().toLowerCase();
+            stopW.insert(stopWord);
+        }
+    } catch (IOException e) {
+        System.out.println("Error reading the file: " + stopWordsFile);
+        e.printStackTrace(); 
+    } finally {
+        if (scanner != null) {
+            scanner.close();  
+        }
+    }
+}
+
+    private String removePunctuation(String word) {
+        return word.replaceAll("[^a-zA-Z0-9]", "");
+    }
+
+    public void preprocessContent(String content, LinkedList<String> WordsLL, int id) {
+       
+        String[] words = content.toLowerCase().split("\\s+");
+
+        for (int i = 0; i < words.length; i++) {
+            String word = removePunctuation(words[i]);
+            if (!isStopWord(word) && !word.isEmpty()) {
+                WordsLL.insert(word);
+                invertedind.addWord(id, word);
+            }
+        }
+        
+    }
+
+    private  boolean isStopWord(String word) {
+        stopW.findFirst();
+        while (stopW.retrieve() != null) {
+            if (stopW.retrieve().equals(word)) {
+                return true;
+            }
+            stopW.findNext();
+        }
+        return false;
+    }
+public LinkedList<String> WordsLLMethod (String content, int id){
+    LinkedList <String> WordsLL = new LinkedList<String>();
+     preprocessContent(content, WordsLL, id);
+    return WordsLL;
+}
+
+    public  void readDocuments(String fileName) {
+        try {
+           
+            Scanner scan = new Scanner(new File(fileName));
+            scan.nextLine();
+
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine();
+                String id = line.substring(0, line.indexOf(',')).trim();
+                int docId = Integer.parseInt(id);
+                String docContent = line.substring(line.indexOf(',') + 1).trim();
+
+                LinkedList<String> WordsLL = WordsLLMethod(docContent, docId);
+                index.addDocument(new Document (docId,WordsLL));
+                
+               // Document document = new Document(docId, wordsList);
+
+               // System.out.print(  document.docuID +" ");
+              //  System.out.println(document.Words.displayWordsInline());
+            }
+            scan.close();
+        } catch (Exception e) {
+            System.out.println("end of file");
+        }
+    }
+
+    public void LoadF(String StopW, String Doc){
+        loadStopWords(StopW);
+                 readDocuments(Doc);
+    }
+    public static void main(String[] args) {
+      Processor p = new Processor();
+      p.LoadF("stop.txt", "dataset.csv");
+              p.index.displayDocs();
+              System.out.println("-\n----------------\n");
+              p.invertedind.displayInvertedIndex();
+        
+    }
+  
+    
+}
