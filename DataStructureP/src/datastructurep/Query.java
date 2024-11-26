79f0414d946162b8b9dcdac1b7bcac0e97b5dea2
lameessa <444201177@student.ksu.edu.sa>
2024-11-26 19:49:28 +0300

    Query

--- DataStructureP/src/datastructurep/Query.java
+++ DataStructureP/src/datastructurep/Query.java
@@ -3,31 +3,98 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package datastructurep;
+
 /**
  *
- * @author nouraalmadhi
+ * @author ghade
  */
-
 public class Query {
     
-static InvertedIndexBST invertedBST;
-
- public Query (InvertedIndexBST invertedBST){
+   static InvertedIndex inverted;
+   static InvertedIndexBST invertedBST;
+   
+   public Query (InvertedIndex inverted){
+       this.inverted=inverted;
+      
+   }
+   
+   
+   public Query (InvertedIndexBST invertedBST){
  
        this.invertedBST=invertedBST;
    }
- 
- public static LinkedList<Integer> BooleanQuery(String Query){
-    if(!Query.contains("AND") && !Query.contains("OR"))
-        return andQuery(Query);
-    else if(Query.contains("AND") && !Query.contains("OR"))
-        return andQuery(Query);
-    else if(!Query.contains("AND") && Query.contains("OR"))
-        return ORQuery(Query);
-    else
-        return MixedQuery(Query);
+   
+public static LinkedList<Integer> andQuery(String Query) {
+    LinkedList<Integer> A = new LinkedList<>();
+    LinkedList<Integer> B = new LinkedList<>();
+    String terms[] = Query.split("AND");
+
+    if (terms.length == 0) {
+        System.out.println("No terms in query.");
+        return A; // Return an empty list
+    }
+
+
+    // Process the first term
+    boolean found = inverted.indexWordEntry(terms[0].trim().toLowerCase());
+    if (found) {
+        A = inverted.invertedList.retrieve().getDocIds();
+        System.out.println("Doc IDs for term: " + terms[0].trim().toLowerCase());
+    } else {
+        System.out.println("Term '" + terms[0].trim().toLowerCase() + "' not found in index.");
+        return A; // Return empty if the first term is not found
+    }
+
+    // Process remaining terms
+    for (int i = 1; i < terms.length; i++) {
+     
+        found = inverted.indexWordEntry(terms[i].trim().toLowerCase());
+        if (found) {
+            B = inverted.invertedList.retrieve().getDocIds();
+            System.out.println("Doc IDs for term: " + terms[i].trim().toLowerCase() );
+        } else {
+            System.out.println("Term '" + terms[i].trim().toLowerCase() + "' not found in index.");
+            return new LinkedList<>(); // Return empty if any term is not found
+        }
+        A = andQuery(A, B); // Perform intersection
+    }
+
+    return A;
 }
-   public static boolean existsInResult(LinkedList<Integer> list, Integer element) {
+
+   
+     public static LinkedList<Integer>andQuery(LinkedList<Integer>A, LinkedList<Integer>B){
+         
+         LinkedList<Integer> result = new LinkedList<Integer>();
+         if(A.isEmpty()||B.isEmpty())
+             return result;
+         
+         A.findFirst();
+         while (true){
+             boolean found = existsInResult(result,A.retrieve());
+             if (!found){
+                 B.findFirst();
+                 while( true){
+                     if (B.retrieve().equals(A.retrieve()))
+                     {
+                 result.insert(A.retrieve());
+                 break;} 
+                     if(!B.last())
+                         B.findNext();
+                     else
+                         break;
+                 }
+             }     
+              if (!A.last())
+                  A.findNext();
+              else
+                  break;
+         } 
+         return result;
+                 
+             }
+     
+  public static boolean existsInResult(LinkedList<Integer> list, Integer element) {
     if (list.isEmpty()) {
         return false; // No elements to check
     }
@@ -39,25 +106,23 @@ static InvertedIndexBST invertedBST;
     }
     return list.retrieve() != null && list.retrieve().equals(element); // Final check
 }
-   
-   
-   
-public static LinkedList<Integer> ORQuery(String query) {
+
+   public static LinkedList<Integer> ORQuery(String query) {
     LinkedList<Integer> A = new LinkedList<>();
     LinkedList<Integer> B = new LinkedList<>();
     String terms[] = query.split("OR");
 
     if (terms.length == 0) return A;
 
-    boolean found = invertedBST.searchWord(terms[0].trim().toLowerCase());
+    boolean found = inverted.indexWordEntry(terms[0].trim().toLowerCase());
     if (found) {
-        A = invertedBST.invertedList.retrieve().getDocIds();
+        A = inverted.invertedList.retrieve().getDocIds();
     }
 
     for (int i = 1; i < terms.length; i++) {
-        found = invertedBST.searchWord(terms[i].trim().toLowerCase());
+        found = inverted.indexWordEntry(terms[i].trim().toLowerCase());
         if (found) {
-            B = invertedBST.invertedList.retrieve().getDocIds();
+            B = inverted.invertedList.retrieve().getDocIds();
             A = ORQuery(A, B); // Perform OR operation between A and B
         }
     }
@@ -100,74 +165,17 @@ public static LinkedList<Integer> ORQuery(LinkedList<Integer> A, LinkedList<Inte
     return result; 
 }
 
-public static LinkedList<Integer> andQuery(String Query) {
-    LinkedList<Integer> A = new LinkedList<>();
-    LinkedList<Integer> B = new LinkedList<>();
-    String terms[] = Query.split("AND");
 
-    if (terms.length == 0) {
-        System.out.println("No terms in query.");
-        return A; // Return an empty list
-    }
-
-
-    // Process the first term
-    boolean found = invertedBST.searchWord(terms[0].trim().toLowerCase());
-    if (found) {
-        A = invertedBST.invertedList.retrieve().getDocIds();
-        System.out.println("Doc IDs for term [" + terms[0].trim().toLowerCase()+"]");
-    } else {
-        System.out.println("Term '" + terms[0].trim().toLowerCase() + "' not found in index.");
-        return A; // Return empty if the first term is not found
-    }
-
-    // Process remaining terms
-    for (int i = 1; i < terms.length; i++) {
-     
-        found = invertedBST.searchWord(terms[i].trim().toLowerCase());
-        if (found) {
-            B = invertedBST.invertedList.retrieve().getDocIds();
-            System.out.println("Doc IDs for term [" + terms[i].trim().toLowerCase()+"]" );
-        } else {
-            System.out.println("Term '" + terms[i].trim().toLowerCase() + "' not found in index.");
-            return new LinkedList<>(); // Return empty if any term is not found
-        }
-        A = andQuery(A, B); // Perform intersection
-    }
-
-    return A;
+public static LinkedList<Integer> BooleanQuery(String Query){
+    if(!Query.contains("AND") && !Query.contains("OR"))
+        return andQuery(Query);
+    else if(Query.contains("AND") && !Query.contains("OR"))
+        return andQuery(Query);
+    else if(!Query.contains("AND") && Query.contains("OR"))
+        return ORQuery(Query);
+    else
+        return MixedQuery(Query);
 }
-
-public static LinkedList<Integer>andQuery(LinkedList<Integer>A, LinkedList<Integer>B){
-         
-         LinkedList<Integer> result = new LinkedList<Integer>();
-         if(A.isEmpty()||B.isEmpty())
-             return result;
-         
-         A.findFirst();
-         while (true){
-             boolean found = existsInResult(result,A.retrieve());
-             if (!found){
-                 B.findFirst();
-                 while( true){
-                     if (B.retrieve().equals(A.retrieve()))
-                     {
-                 result.insert(A.retrieve());
-                 break;} 
-                     if(!B.last())
-                         B.findNext();
-                     else
-                         break;
-                 }
-             }     
-              if (!A.last())
-                  A.findNext();
-              else
-                  break;
-         } 
-         return result;
-                 
-             }
 
 
 public static LinkedList<Integer> MixedQuery(String Query){
